0c49b408fc9647a2236286a40971d2f7a517645e
mswift42 <martin.haesler@gmail.com>
2013-03-04 19:51:01 +0000

    added 15 - 20.

--- ninetynine.hs
+++ ninetynine.hs
@@ -57,12 +57,48 @@ pr12 [] = []
 pr12 (x:xs) = replicate (fst x) (snd x) ++ pr12 xs
 
 
+-- List decoding directly
 pr13 [] = []
 pr13 xs = let groupedlist = group xs
           in if length (head groupedlist) > 1
              then (length (head groupedlist),head groupedlist):
                   pr13 (tail xs)
              else (1,head groupedlist) : pr13 (tail xs)
+
+
+-- Duplicate the elements of a list.
+pr14 [] = []
+pr14 (x:xs) = replicate 2 x ++ pr14 xs
+
+
+-- replicate elements of a list a given amount.
+pr15 _ [] = []
+pr15 n (x:xs) = replicate n x ++ pr15 n xs
+
+
+-- drop every nth element of a list.
+pr16 _ [] = []
+pr16 0 xs = xs
+pr16 1 xs = []
+pr16 n xs = take (n-1) xs ++ pr16 n (drop n xs)
+
+-- split list into two parts. Length of first part is given.
+pr17 _ [] = []
+pr17 0 xs = [xs]
+pr17 n xs = [take n xs] ++ [drop n xs]
+
+-- extract a slice from a list.
+pr18 xs s e = [xs !! i | i <- [s-1..e-1]]
+
+pr19 [] _ = []
+pr19 xs 0 = xs
+pr19 xs n | n > 0 = drop n xs ++ take 3 xs
+          | n < 0 = drop (length xs + n) xs ++ take (length xs + n) xs
+
+-- remove the k-th element from a list.
+pr20 1 (x:xs) = (x, xs)
+pr20 n (x:xs) = (a, x:ys)
+	where (a, ys) = pr20 (n - 1) xs
 
 
 
